14cc314738bf6469ed695103798a724d947ee001
Michelle JL <michelle@michellejl.com>
2017-08-30 15:00:53 -0700

    manually create grid

--- designs.js
+++ designs.js
@@ -3,8 +3,27 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(height, width) {
+  var table = document.getElementById("pixel_canvas");
+  var grid = '';
 
-// Your code goes here!
+  // loop over each row
+  for (var i = 0; i < height; i++){
+    grid += '<tr class="row-' + i + '">'
+    // loop for each cell
+    for (var j = 0; j < width; j++){
+      grid += '<td class="cell" id="row-' + i + '_cell-' + j + '"></td>'
+    }
+    grid += '</tr>'
+  }
 
+  // add grid into table element
+  table.innerHTML = grid
 }
+
+// on submit of form #sizePicker:
+  // height = value of #input_height
+  // width = value of #input_width
+  // makeGrid(height, width);
+
+makeGrid(5, 5);
